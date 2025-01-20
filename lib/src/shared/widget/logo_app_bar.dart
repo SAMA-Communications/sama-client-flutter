b02cc0b7ca101719619d12a9db0cf43cfe6b72c0
Magellan <magellan@connectycube.com>
2025-01-20 18:57:36 +0200

    add deleteAllData

--- lib/src/shared/widget/logo_app_bar.dart
+++ lib/src/shared/widget/logo_app_bar.dart
@@ -2,8 +2,10 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 
+import '../secure_storage.dart';
 import '../ui/colors.dart';
 import '../utils/screen_factor.dart';
+import 'multi_gesture_detector.dart';
 
 class LogoAppBar extends StatelessWidget implements PreferredSizeWidget {
   const LogoAppBar({super.key});
@@ -12,27 +14,52 @@ class LogoAppBar extends StatelessWidget implements PreferredSizeWidget {
   Widget build(BuildContext context) {
     return AppBar(
         flexibleSpace: FlexibleSpaceBar(
-      background: Stack(
-        children: [
-          Image.asset(
-            'assets/images/sama_background.png',
-            width: double.infinity,
-            fit: BoxFit.cover,
-          ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Container(
-              height: 46,
-              decoration: const BoxDecoration(
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(24),
-                  topRight: Radius.circular(24),
-                ),
-                color: white,
+      background: MultiGestureDetector(
+        taps: 5,
+        onTap: () => showDialog(
+          context: context,
+          builder: (_) => AlertDialog(
+            title: const Text('Erase all data'),
+            content: const Text('Are you sure you want to erase all data?'),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text('Cancel'),
               ),
+              TextButton(
+                onPressed: () {
+                  SecureStorage.instance.deleteAllData();
+                  Navigator.of(context).pop();
+                },
+                child: const Text('OK'),
+              )
+            ],
+          ),
+        ),
+        child: Stack(
+          children: [
+            Image.asset(
+              'assets/images/sama_background.png',
+              width: double.infinity,
+              fit: BoxFit.cover,
             ),
-          )
-        ],
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                height: 46,
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(24),
+                    topRight: Radius.circular(24),
+                  ),
+                  color: white,
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     ));
   }
